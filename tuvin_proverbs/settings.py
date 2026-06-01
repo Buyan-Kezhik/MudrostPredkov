da5eeee486a5bdd9f3d85b3a2b9a0e1b43fe6ca2
Buyan-Kezhik <saayabuyan-kezhik@yandex.ru>
2026-06-01 21:00:48 +0700

    Switch back to SQLite for Render

--- tuvin_proverbs/settings.py
+++ tuvin_proverbs/settings.py
@@ -91,10 +91,10 @@ WSGI_APPLICATION = 'tuvin_proverbs.wsgi.application'
 # =============================================================================
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://neondb_owner:npg_PgOTMt2HD6Lf@ep-small-cherry-aqev4t0h-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # =============================================================================
